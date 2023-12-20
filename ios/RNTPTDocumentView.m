a490c497d03fb9cf2bec2e69936571ceb4d0837f
HanHuynh91 <luong.ha-quoc@banvien.com.vn>
2023-12-20 09:21:46 +0700

    Fixbug iOS

--- ios/RNTPTDocumentView.m
+++ ios/RNTPTDocumentView.m
@@ -219,9 +219,14 @@ NS_ASSUME_NONNULL_END
     if (@available(iOS 13.0, *)) {
         UINavigationBarAppearance *customNavBarAppearance = [[UINavigationBarAppearance alloc] init];
         
-        // Apply a white background.
+        NSString *hexColorString = @"F8F8F8";
+        
+        UIColor *backgroundBarColor = [self colorWithHexString:hexColorString ];
+        
+        // Apply color navigation bar background.
         [customNavBarAppearance configureWithOpaqueBackground];
-        customNavBarAppearance.backgroundColor = [UIColor whiteColor];
+        UIColor *backgroundColor = backgroundBarColor;
+        customNavBarAppearance.backgroundColor = backgroundColor;
         
         // Apply white colored normal and large titles.
         customNavBarAppearance.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
@@ -232,6 +237,42 @@ NS_ASSUME_NONNULL_END
         // Handle cases where iOS version is less than 13.0 if needed
         return nil;
     }
+}
+
+- (UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+
+    if ([cString length] != 6) return  [UIColor grayColor];
+
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                          green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                          alpha:1.0f];
 }
 
 - (void)unloadDocumentViewController
